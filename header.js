e6c78249ff697859040f8792f592a4c1241eeb09
Aleksandr Haruzin <119446433+skaller-hub@users.noreply.github.com>
2024-03-30 01:59:17 +0200

    Update header.js

--- header.js
+++ header.js
@@ -10,10 +10,10 @@ document.addEventListener('DOMContentLoaded', function() {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/offerstallinn.html">Туры из Таллинна</a>
+            <a class="nav-link active" aria-current="page" href="offerstallinn.html">Туры из Таллинна</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/offersriga.html">Туры из Риги</a>
+            <a class="nav-link active" aria-current="page" href="offersriga.html">Туры из Риги</a>
           </li>
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="recommendhotels.html">Рекомендуемые отели</a>
