b72385472ef1df6f4d65e23d78f5cfbdb89b3567
Aleksandr Haruzin <119446433+skaller-hub@users.noreply.github.com>
2024-03-30 02:02:54 +0200

    Update header.js

--- header.js
+++ header.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var navbar = `
     <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/index.html"><img src="./img/logo.png" alt="Логотип"></a>
+      <a class="navbar-brand" href="index.html"><img src="./img/logo.png" alt="Логотип"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Переключатель навигации">
         <span class="navbar-toggler-icon"></span>
